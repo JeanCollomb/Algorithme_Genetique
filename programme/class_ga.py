17c75de496a59aa4b269508f89361d153feea94c
JeanCollomb <40898416+JeanCollomb@users.noreply.github.com>
2018-07-06 21:03:32 +0200

    fonction mutation
    Ajout de la fonction mutation

--- programme/class_ga.py
+++ programme/class_ga.py
@@ -24,6 +24,9 @@ class Algorithme_Genetique () :
         '''
         self.nombre_individus         = parametres_algorithme[0]
         self.nombre_generation        = parametres_algorithme[1]
+        self.probabilite_mutation     = parametres_algorithme[2]
+        self.probabilite_croisement   = parametres_algorithme[3]
+        self.probabilite_ellitisme    = parametres_algorithme[4]
         
         self.parametres_discrets      = parametres_produit[0]
         self.parametres_continus      = parametres_produit[1]
@@ -32,6 +35,9 @@ class Algorithme_Genetique () :
         self.population_initiale     = zeros((self.nombre_individus, self.nombre_fonction + len(self.parametres_discrets) + len(self.parametres_continus)))
         self.population_generation_old= zeros((self.nombre_individus, self.nombre_fonction + len(self.parametres_discrets) + len(self.parametres_continus)))
         self.population_generation    = zeros((self.nombre_individus, self.nombre_fonction + len(self.parametres_discrets) + len(self.parametres_continus)))
+        self.nombre_individus_mutation= int(round(self.nombre_individus * self.probabilite_mutation,0))
+        self.nombre_individus_croisement= int(round(self.nombre_individus * self.probabilite_croisement,0))
+        self.nombre_individus_ellistisme= int(round(self.nombre_individus * self.probabilite_ellitisme,0))
     
     ###---------------------------------------------------------------------###
     def fct_initialisation_population (self) :
@@ -48,15 +54,45 @@ class Algorithme_Genetique () :
             
             for continu in range(len(self.parametres_continus)):
                 self.population_initiale[individu][len(self.parametres_discrets) + continu] = uniform(self.parametres_continus[continu][0], self.parametres_continus[continu][1])
-            
+        
+        self.population_generation_old = self.population_initiale
         
     ###---------------------------------------------------------------------###
     def fct_mutation (self) :
         '''
+        Fonction permettant de creer des individus mutes de manière aléatoire 
+        dans la nouvelle génération.
+        '''
+        
+        for individu in range(len(self.population_generation)):
+            
+            for discret in range(len(self.parametres_discrets)):
+                test_proba = randint(0, 100)
+                if test_proba <= self.probabilite_mutation :
+                    self.population_generation[individu][discret] = randint(0, len(self.parametres_discrets[discret]) - 1)
+                
+            for continu in range(len(self.parametres_continus)):
+                test_proba = randint(0, 100)
+                if test_proba <= self.probabilite_mutation :
+                    self.population_generation[individu][len(self.parametres_discrets) + continu] = uniform(self.parametres_continus[continu][0], self.parametres_continus[continu][1])
+            
+    def fct_croisement (self) :
+        '''
         '''
         
         pass
-
+    
+    def fct_selection_ellistisme (self) :
+        '''
+        '''
+        
+        pass
+    
+    def fct_selection_duel (self) :
+        '''
+        '''
+        
+        pass
 
 
 
--- programme/main_exemple.py
+++ programme/main_exemple.py
@@ -14,16 +14,26 @@ from class_ga import Algorithme_Genetique
 ###############################################################################
 ###----> Parametres
 
-nombre_individus         = 10
+nombre_individus         = 100
 nombre_generation        = 20
+probabilite_mutation     = 0.05
+probabilite_croisement   = 0.25
+probabilite_ellitisme    = 0.1
 
 parametres_discrets      = [['eau', 'huile'], ['acier', 'aluminium', 'cuivre']]
 parametres_continus      = [[1, 10], [0, 100], [0.3, 0.8]]
 
 nombre_fonctions         = 1
 
-parametres_algorithme    = [nombre_individus, nombre_generation]
-parametres_produit       = [parametres_discrets, parametres_continus, nombre_fonctions]
+parametres_algorithme    = [nombre_individus, 
+                            nombre_generation, 
+                            probabilite_mutation, 
+                            probabilite_croisement, 
+                            probabilite_ellitisme]
+
+parametres_produit       = [parametres_discrets, 
+                            parametres_continus, 
+                            nombre_fonctions]
 
 ###############################################################################
 ###----> Initialisation
@@ -33,7 +43,12 @@ exemple = Algorithme_Genetique(parametres_algorithme, parametres_produit)
 ###############################################################################
 ###----> Population initiale
 
+exemple.fct_initialisation_population()
+exemple.population_initiale
+exemple.population_generation_old
 
+exemple.fct_mutation()
+a= exemple.population_generation
 
 ###############################################################################
 ###############################################################################
